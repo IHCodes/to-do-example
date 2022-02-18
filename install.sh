e6657e3c89274a48f061933ad7233505b0011fb1
Imam Hussain <97616881+IHCodes@users.noreply.github.com>
2022-02-18 16:53:01 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,9 +6,9 @@ sudo apt install python3
 echo "installing pip"
 sudo apt install python3-pip
 echo "creating virtual environment"
-python3.8-distutils
+sudo python3.8-distutils
 sudo apt install python3.8-venv
-python3 -m venv .venv
+sudo python3 -m venv .venv
 source .venv/bin/activate
 echo "installing project requirements"
 pip install -r requirements.txt
